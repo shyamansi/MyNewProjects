58cc63f398854e45bb5c49dd4755c616992ebb8b
shyamansi <66917662+shyamansi@users.noreply.github.com>
2024-11-02 13:25:46 +0530

    second case is successfully  passed

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,5 +1,6 @@
 class StringCalculator
   def self.add(input)
     return 0 if input.empty?
+    input.to_i
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,5 +5,8 @@ RSpec.describe StringCalculator do
 		it "return 0 for an empty string" do
 		expect(StringCalculator.add("")).to eq(0)
 	end
+	    it "return number itself for single number input" do  
+         expect(StringCalculator.add("1")).to eq(1)
+	    end
 end
 end
