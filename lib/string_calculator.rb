7b2e2f20fa6256c272f56dc17914f6a8039fa333
shyamansi <66917662+shyamansi@users.noreply.github.com>
2024-11-02 13:35:08 +0530

    third test case has been successfully  passes

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,7 @@
 class StringCalculator
   def self.add(input)
     return 0 if input.empty?
-    input.to_i
+    number  = input.split(",").map(&:to_i)
+    number.sum
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,12 +1,15 @@
 require "string_calculator"
 require 'spec_helper'
 RSpec.describe StringCalculator do    
-	describe ".add" do  
-		it "return 0 for an empty string" do
-		expect(StringCalculator.add("")).to eq(0)
-	end
-	    it "return number itself for single number input" do  
-         expect(StringCalculator.add("1")).to eq(1)
-	    end
-end
+		describe ".add" do  
+			it "return 0 for an empty string" do
+			expect(StringCalculator.add("")).to eq(0)
+			end
+		    it "return number itself for single number input" do  
+		     expect(StringCalculator.add("1")).to eq(1)
+		    end
+		    it "return sum of two comma-separated numbers" do 
+		    	expect(StringCalculator.add("1,6")).to eq(7)
+		    end
+		end
 end
